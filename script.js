fd405a51d7e98d6cb1d082644e65b29ba73b5598
Mercy Wacera <waceraaamercy@gmail.com>
2025-06-21 01:54:39 -0700

    Added Skill

--- script.js
+++ script.js
@@ -4,4 +4,27 @@ function scrollToSection(id) {
       section.scrollIntoView({ behavior: 'smooth' });
     }
   }
+
+
+  function openImageModal(src) {
+    const modal = document.getElementById("imageModal");
+    const modalImg = document.getElementById("modalImage");
+    modal.style.display = "flex";
+    modalImg.src = src;
+  }
+  
+  function closeImageModal() {
+    document.getElementById("imageModal").style.display = "none";
+  }
+  
+  
+
+  function downloadCV() {
+    const link = document.createElement('a');
+    link.href = 'files/Wacera_CV.pdf';
+    link.download = 'Wacera_CV.pdf'; // name of the file when downloaded
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
   
